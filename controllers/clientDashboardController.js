cfc0e9baff23465cf7a31d428377397e023cebb7
Hritesh <hsworkmail.1@gmail.com>
2024-10-15 04:00:31 +0530

    Update CRUD functionality

--- controllers/clientDashboardController.js
+++ controllers/clientDashboardController.js
@@ -1,6 +1,16 @@
-const clientDashboard = (req, res) => {
-  const userName = req.session.user.firstName
-  res.render('clientDashboardView', { userName: userName })
+const { project } = require('../models')
+
+const clientDashboard = async (req, res) => {
+  try {
+    const userName = req.session.user.firstName
+
+  const projects = await project.findAll({where:{clientUserId:req.session.user.id}})
+  res.render('clientDashboardView', { userName: userName, projects: projects })
+  }catch(err){
+    console.error('error while fetching data', err);
+    
+  }
+  
 }
 
 module.exports = clientDashboard
--- controllers/projectCardController.js
+++ controllers/projectCardController.js
@@ -1,8 +1,13 @@
-const projectCards = (req, res)=> {
-  res. render('projectCardView')
+const { project } = require('../models')
+
+const projectCards = async (req, res)=> {
+  try{
+    const allProject = await project.findAll()
+    res. render('projectCardView', {allProject: allProject})
+  } catch(err) {
+    console.error("error while printing all projects", err);
+  }
 }
-
-
 
 
 module.exports = projectCards
